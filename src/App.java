a0d07aba54eae80ff112125a77b09ef49112be99
petrdragonman <petrdragonman@me.com>
2025-02-04 14:37:06 +1100

    implements file set up

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int i = 0;
+        while(i < 10) {
+            System.out.println("+---+");
+            System.out.println("|   |");
+            System.out.println("+---+");
+            i++;
+        }
+        System.out.println("Welcome to minesweeper!");
+        System.out.println("press 'I' to read the rules");
+        System.out.println("press 'P' to play");
+        System.out.println("press 'Q' to quit");
     }
 }
