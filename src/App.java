d620c48c881f8c625519ffc57661207c0a1c284e
petrdragonman <petrdragonman@me.com>
2025-02-04 22:56:03 +1100

    implements game loop

--- src/App.java
+++ src/App.java
@@ -1,51 +1,38 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("");
-        System.out.println("     A   B   C   D   E   F   G   H   I   J  ");
-        for(int i = 1; i <= 10; i++) {
-            if(i < 10) {
-                System.out.println("   +---+---+---+---+---+---+---+---+---+---+");
-                System.out.println(i+"  |   |   | 1 |   |   |   |   |   |   |   |");
-            } else {
-                System.out.println("   +---+---+---+---+---+---+---+---+---+---+");
-                System.out.println(i+" |   |   |   |   |   |   |   |   |   |   |");
-            }
-        }
-        System.out.println("   +---+---+---+---+---+---+---+---+---+---+");
-        System.out.println("");
-
-
-        System.out.println("Welcome to minesweeper!");
+        boolean running = false;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What is your name: ");
+        String name = scan.nextLine();
+        System.out.println("Hi " + name + "! Do you want to play minesweeper?");
         System.out.println("press 'I' to read the rules");
         System.out.println("press 'P' to play");
         System.out.println("press 'Q' to quit");
+        String option = scan.nextLine();
+        if(option.equals("p")) {
+            running = true;
+        }
+        
+        while(running) {
+            System.out.println("press 'I' to read the rules");
+            System.out.println("press 'P' to play");
+            System.out.println("press 'Q' to quit");
+            
+            System.out.println("Enter option: ");
+            option = scan.nextLine();
+            System.out.println(option);
+            if(option.equals("q")) {
+                System.out.println("quittingi game....");
+                running = false;
+            }
+            //if(option.equals("p")) {
+                Grid.drawGrid();
+            //}
+            
+        }
+        System.out.println("GoodBye!");
+        scan.close();
     }
 }
-
-//System.out.println("+---+");
-//System.out.println("|   |");
-//System.out.println("+---+");
-
-/**
- * for(int i = 1; i < 10; i++) { //columns
-            //System.out.print("+");
-            for(int j = 0; j < 41; j++) { //rows
-                if(j%4 == 0) {
-                    System.out.print("+");
-                    if(j == 40) {
-                        System.out.println("");
-                    }
-                }else {
-                    System.out.print("-");
-                }
-                //System.out.print("+");
-            //     if(j%4 == 0){
-            //         System.out.print("+");
-            //     } else {
-            //         System.out.println("");
-            //         System.out.print("-");
-            //    }
-                
-            }
-        }
- */
